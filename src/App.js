6f174ece540dcf0ec13995b468783b3cfe76203b
S.Mirza <mirzafrontdev@gmail.com>
2025-07-11 13:00:54 +0200

    commit two

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import './App.css';
 
+const $valueText = {
+  persone: {
+    name1: 'Jessica Randall',
+    city: 'London, Unite Kingdom',
+    job: ' "Front-end developer and avid reader." '
+  },
+   socilas: {
+    git: 'GitHub',
+    mentor: 'Front-end Mentor',
+    linkedin: 'Linkeldin',
+    twitter: 'Twitter',
+    instagram: 'Instagram'
+  }
+}
+
 function App() {
   return (
     <main className='main'>
@@ -22,9 +37,9 @@ function App() {
 function AuthorBox() {
   return(
     <div className='author__box'>
-      <h3 className='name'>Jessica Randall</h3>
-      <p className='location'>London, Unite Kingdom</p>
-      <p className='job'>"Front-end developer and avid reader."</p>
+      <h3 className='name'>{$valueText.persone.name1}</h3>
+      <p className='location'>{$valueText.persone.city}</p>
+      <p className='job'>{$valueText.persone.job}</p>
     </div>
   )
 }
@@ -33,11 +48,11 @@ function LinksBio() {
   return (
     <div className='btn__box'>
       <ul className='list'>
-        <li><a href='#git' className='btn'>GitHub</a></li>
-        <li><a href='#front' className='btn'>Front-end Mentor</a></li>
-        <li><a href='#link' className='btn'>Linkeldin</a></li>
-        <li><a href='#twwiter' className='btn'>Twitter</a></li>
-        <li><a href='#instagram' className='btn'>Instagram</a></li>
+        <li><a href='#git' className='btn'>{$valueText.socilas.git}</a></li>
+        <li><a href='#front' className='btn'>{$valueText.socilas.mentor}</a></li>
+        <li><a href='#link' className='btn'>{$valueText.socilas.linkedin}</a></li>
+        <li><a href='#twitter' className='btn'>{$valueText.socilas.twitter}</a></li>
+        <li><a href='#instagram' className='btn'>{$valueText.socilas.instagram}</a></li>
       </ul>
     </div>
   )
